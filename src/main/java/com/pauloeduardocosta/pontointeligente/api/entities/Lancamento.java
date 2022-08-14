9d14b9b9ade895d8704bb360da1c51e509138f65
Eduardo32 <p.eduardo.32@gmail.com>
2022-08-14 20:17:27 -0300

    Mapeando entidades

---
+++ src/main/java/com/pauloeduardocosta/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,71 @@
+package com.pauloeduardocosta.pontointeligente.api.entities;
+
+import com.pauloeduardocosta.pontointeligente.api.enums.ETipo;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@ToString
+@Entity
+@Table(name = "lancamentos")
+public class Lancamento implements Serializable {
+
+    private static final long serialVersionUID = 6524560251526772839L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data", nullable = false)
+    private Date data;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "localizacao")
+    private String localizacao;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private Date dataAtualizacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private ETipo tipo;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Funcionario funcionario;
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+}
---
+++ src/main/java/com/pauloeduardocosta/pontointeligente/api/enums/EPerfil.java
@@ -0,0 +1,6 @@
+package com.pauloeduardocosta.pontointeligente.api.enums;
+
+public enum EPerfil {
+    ROLE_ADMIN,
+    ROLE_USUARIO;
+}
